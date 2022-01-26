f14badb6fb91ffcf034d1ff3f0022087abc2add9
gamanedy144 <gamanedy@yahoo.com>
2022-01-26 14:02:30 +0200

    Solve bugs after pressing equal. Add functionality for AC, C and . keys

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function operate(operation, value1, value2){
             return multiply(value1,value2);
         case 'divide':
             return divide(value1, value2);
-        case 'percentage':
+        case 'remainder':
             return remainder(value1, value2);
         default:
             return 0;
@@ -35,13 +35,19 @@ function remainder(value1, value2){
 const curr = document.querySelector('#current');
 const prev = document.querySelector('#previous');
 const digits = document.querySelectorAll('.digit');
+const hiddenP = document.querySelector('.hidden');
 let digitPressed = false;
 let operationDoneBefore = false;
+let exceptions = 'divide multiply remainder';
 digits.forEach(digit => {
     digit.addEventListener('click', e =>{
         if(equalPressed == true){
-            prev.textContent = `&nbsp;`;
+            hiddenP.classList.remove('hidden');
+            prev.textContent = "";
+            prevValue = 0;
             equalPressed = false;
+            operationDoneBefore = false;
+            dotPreviouslyPressed = false;
             console.log('I am here');
         }       // FIX DIGIT WHEN EQUAL WAS PRESSED BEFORE
         if(digit.dataset.value == 0 && digitPressed == false){
@@ -71,7 +77,8 @@ operations.forEach(operation =>{
         
         let functionName = operation.dataset.value;
         currValue = parseFloat(curr.textContent);
-        console.log(`currValue = ${currValue}   prevValue = ${prevValue} `)
+        console.log(`START currValue = ${currValue}   prevValue = ${prevValue} \n prevFunctionName = ${prevFunctionName} digitPressed = ${digitPressed} equalPressed = ${equalPressed}`);
+        console.log(`operationDoneBefore = ${operationDoneBefore}`);
 
         if(prevFunctionName == 'divide' && currValue == 0){
             prev.textContent = parseFloat(prev.textContent);
@@ -81,7 +88,6 @@ operations.forEach(operation =>{
             prev.textContent = `${curr.textContent}`;
             
         }
-    
         else if(equalPressed == false){
             let operationResult = operate(prevFunctionName, prevValue, currValue)
             prev.textContent = `${operationResult}`;
@@ -94,10 +100,15 @@ operations.forEach(operation =>{
         prevValue = parseFloat(prev.textContent);
         prev.textContent +=  ` ${operation.dataset.key} `;
         curr.textContent = "0";
+        currValue = parseFloat(curr.textContent);
         digitPressed = false;
         operationDoneBefore = true;
         prevFunctionName = functionName;
         lastOperation = operation;
+        dotPreviouslyPressed = false;
+        hiddenP.classList.add('hidden');
+        console.log(`END currValue = ${currValue}   prevValue = ${prevValue} \n prevFunctionName = ${prevFunctionName} digitPressed = ${digitPressed} equalPressed = ${equalPressed}`)
+        console.log(`operationDoneBefore = ${operationDoneBefore}`);
     })
 });
 let equalPressed = false;
@@ -109,12 +120,84 @@ equal.addEventListener('click', e =>{
         prev.textContent = `${prevValue} ${lastOperation.dataset.key} ${currValue} =`;
         curr.textContent = operate(prevFunctionName, prevValue, currValue);
         prevValue = parseFloat(curr);
+        if(prevValue % 1 == 0){
+            dotPreviouslyPressed = false;
+        }
         equalPressed = true;
         digitPressed = false;
-        
+        operationDoneBefore = false;
+        hiddenP.classList.add('hidden');
     }
     else{
-        prevValue = 0;
+        if(exceptions.includes(prevFunctionName)){
+            prevValue = 1;
+        }
+        else prevValue = 0;
     }
+
     return 0;
 });
+
+
+const deleteOne = document.querySelector('#delete');
+
+deleteOne.addEventListener('click', e=>{
+    //delete the last character of the current number entered
+    //if there is no number entered, do nothing
+    //if there was a previous value entered, delete the last character of that number
+    console.log('hello');
+    if(digitPressed == true){
+        curr.textContent = curr.textContent.slice(0,-1);
+    }
+    else{
+        curr.textContent = ("" + parseFloat(prev.textContent)).slice(0,-1);
+        prev.textContent ="";
+        hiddenP.classList.remove('hidden');
+        digitPressed = true;
+    }
+
+});
+
+const dot = document.querySelector('#dot');
+let dotPreviouslyPressed = false;
+dot.addEventListener('click', e=>{
+    if(parseFloat(curr.textContent) == 0 && dotPreviouslyPressed == false){
+        dotPreviouslyPressed = true;
+        curr.textContent +='.';
+        digitPressed = true;
+    }
+    else if(equalPressed == true && dotPreviouslyPressed == false){
+        equalPressed == false;
+        dotPreviouslyPressed = true;
+        digitPressed = true;
+        curr.textContent = parseFloat(curr.textContent) + '.';
+        prev.textContent ="";
+        hiddenP.classList.remove('hidden');
+
+    }
+    else if(digitPressed == true && dotPreviouslyPressed != true){
+        curr.textContent += '.';
+        dotPreviouslyPressed = true;
+    }
+    else if(dotPreviouslyPressed == true){
+        //do nothing
+    }
+    else{
+        curr.textContent = parseFloat(prev.textContent) + '.';
+        prev.textContent ="";
+        hiddenP.classList.remove('hidden');
+        dotPreviouslyPressed = true;
+    }
+});
+
+const allClear = document.querySelector('#clear');
+allClear.addEventListener('click', e=>{
+    hiddenP.classList.remove('hidden');
+    prev.textContent = "";
+    prevValue = 0;
+    equalPressed = false;
+    operationDoneBefore = false;
+    dotPreviouslyPressed = false;
+    digitPressed = false;
+    curr.textContent = '0';
+})
